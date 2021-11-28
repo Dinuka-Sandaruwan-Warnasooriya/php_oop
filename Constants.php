175a6a7928df7f8739700d115ef54ab4d8c7b760
Dinuka-Sandaruwan-Warnasooriya <dinukaswarnasooriya@gmail.com>
2021-11-29 02:08:54 +0530

    Constants Add

---
+++ Constants.php
@@ -0,0 +1,18 @@
+<?php
+class Employee{
+    public const EPF_RATE = 0.08;
+    public  $basic_salary = 50000;
+
+    public function cal_EPF(){
+         return $this->basic_salary * self::EPF_RATE;
+    }
+}
+
+$emp_1 = new Employee();
+
+echo 'EPF Rate' . Employee::EPF_RATE;
+echo '<br>';
+echo 'EPF Amount for emp_1'. $emp_1->cal_EPF();
+
+
+?>
